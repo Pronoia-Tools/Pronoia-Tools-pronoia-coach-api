b250eab2900a64cb1c8197ca8b4bbe7d65179730
IamCocoDev <giovencofede@gmail.com>
2021-09-15 08:18:41 -0300

    Register have firebase and semi authentication

--- api/routes/test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var test = express.Router();
-
-/* GET users listing. */
-test.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = test;
