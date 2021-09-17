34c71d983fa6ef876085d6522713977a37af94ad
Tomas Obregon <tomasobregon@iMac-de-Tomas.lan>
2021-09-17 20:13:13 -0300

    finished login with validation

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const index = express.Router();
 
 // Import routes
 
-const register = require('./register');
-
+const register = require("./register");
+const login = require("./login");
 // Create routing
 
-index.use('/register', register)
+index.use("/register", register);
+index.use("/login", login);
 
 module.exports = index;
