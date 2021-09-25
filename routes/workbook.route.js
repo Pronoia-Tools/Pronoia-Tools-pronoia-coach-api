f6c2831ce34d56cfa15bb3eab122119ff83e56fa
IamCocoDev <fede.giovenco@clonify.online>
2021-09-24 22:58:02 -0300

    Validation semi finished crud not started

--- routes/workbook.route.js
+++ routes/workbook.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const workbookController = require('../controllers/workbook.controller');
 const router = express.Router();
+const workbookValidation = require('../validations/workbook.validation');
 
 router.get('/get', workbookController.get);
 router.post('/post', workbookController.post);
---
+++ validations/workbook.validation.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+const { password } = require('./custom.validation');
+
+const workbook = {
+  body: Joi.object().keys({
+    title: Joi.string().required(),
+    price: Joi.number().required(),
+    edition: Joi.string().required(),
+    categories: Joi.array().items(Joi.string()).required(),
+    description: Joi.string().required(),
+    language: Joi.string().required(),
+    categories: Joi.array().items(Joi.string()).required(),
+  }),
+};
+
+
+module.exports = {
+  workbook,
+};
